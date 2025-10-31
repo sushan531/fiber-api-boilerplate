9bbce378043e0c3471feef079b3716b19b4dc61b
spt-codavatar <sushant.prajapati@codavatar.com>
2025-10-31 13:18:29 +0545

    rename

--- api/endpoints/auth.go
+++ api/endpoints/auth.go
@@ -3,7 +3,7 @@ package endpoints
 import (
 	"database/sql"
 	"fiber-api/api/handlers/handlers"
-	"fiber-api/api/handlers/parsers"
+	"fiber-api/api/handlers/prehandlers"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/sushan531/auth-sqlc/generated"
@@ -12,7 +12,7 @@ import (
 )
 
 func AuthRouter(route fiber.Router, db *sql.DB, queries *generated.Queries, jwkManager manager.JwkManager, tokenService service.TokenService) {
-	route.Post("/signup", parsers.SignupInputParser(db), handlers.UserSignUpHandler(queries, db))
+	route.Post("/signup", prehandlers.SignupInputParser(db), handlers.UserSignUpHandler(queries, db))
 	route.Post("/login", handlers.LoginHandler(queries, jwkManager, tokenService))
 	route.Post("/refresh", handlers.RefreshTokenHandler(queries, tokenService))
 }
